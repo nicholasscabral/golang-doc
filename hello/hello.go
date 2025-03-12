05907324c2d02d111ca6e480cf5244f361ff1fa4
Nicholas Cabral <nicholasscabral@gmail.com>
2025-03-11 22:28:34 -0300

    feat: add hellos function for multiple hello using for

--- hello/hello.go
+++ hello/hello.go
@@ -13,15 +13,29 @@ import (
 )
 
 func main() {
+	// QUOTE
 	fmt.Println(quote.Go())
 
+	// GREETINGS
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 	message, err := greetings.Hello("Nicholas")
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	fmt.Println(message)
+
+	names := []string{"Nicholas", "Cabral", "Dos Anjos"}
+	messages, err := greetings.Hellos(names)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(messages)
+
+	for key, value := range messages {
+		fmt.Println(key, value)
+	}
+
+	// ASDF TEST MODULE
 	asdf.PrintRandomDate()
 }
